2e44994bd7d568b493a080919d033be36f2e2a07
natanzeraa <natanzeraaaaaa@gmail.com>
2024-11-01 11:25:19 -0300

    feat: add transfer controller for creating money transfers between users

---
+++ src/main/java/com/picpaysimplificado/controllers/TransferController.java
@@ -0,0 +1,27 @@
+package com.picpaysimplificado.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.picpaysimplificado.domain.entities.Transfer;
+import com.picpaysimplificado.dtos.TransferDTO;
+import com.picpaysimplificado.services.TransferService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/transfers")
+public class TransferController {
+	@Autowired
+	TransferService transferService;
+
+	@PostMapping
+	public ResponseEntity<Transfer> createNewTransfer(@RequestBody @Valid TransferDTO transfer) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(transferService.createTransfer(transfer));
+	}
+}
