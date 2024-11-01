4dc6c47a6ae8ee9498cbcfc6280a437c16d6bd9d
natanzeraa <natanzeraaaaaa@gmail.com>
2024-11-01 11:26:08 -0300

    feat: add user controller for create, and retrieve new users

---
+++ src/main/java/com/picpaysimplificado/controllers/UserController.java
@@ -0,0 +1,36 @@
+package com.picpaysimplificado.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.picpaysimplificado.domain.entities.User;
+import com.picpaysimplificado.dtos.UserDTO;
+import com.picpaysimplificado.services.UserService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+	@Autowired
+	UserService userService;
+
+	@PostMapping
+	public ResponseEntity<User> createNewUser(@RequestBody @Valid UserDTO user) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(userService.createUser(user));
+	}
+
+	@GetMapping
+	public ResponseEntity<List<User>> findAllUsers() {
+		return ResponseEntity.status(HttpStatus.OK).body(userService.findAllUsers());
+	}
+}
