dc4b74e52c9e347b8c956feada5268aac7f4f8c1
natanzeraa <natanzeraaaaaa@gmail.com>
2024-11-01 11:44:35 -0300

    feat: add TransferService for handling transfer business logic

---
+++ src/main/java/com/picpaysimplificado/services/TransferService.java
@@ -0,0 +1,72 @@
+package com.picpaysimplificado.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.picpaysimplificado.domain.entities.Transfer;
+import com.picpaysimplificado.domain.entities.User;
+import com.picpaysimplificado.records.TransferRecord;
+import com.picpaysimplificado.repositories.TransferRepository;
+import com.picpaysimplificado.repositories.UserRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class TransferService {
+
+	@Autowired
+	UserService userService;
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Autowired
+	TransferRepository transferRepository;
+
+	@Autowired
+	AuthorizationService authorizationService;
+
+	@Autowired
+	NotificationService notificationService;
+
+	@Transactional
+	public Transfer createTransfer(TransferRecord transfer) {
+
+		User sender = userRepository.findById(transfer.sender())
+				.orElseThrow(() -> new RuntimeException("Sender not found"));
+
+		User receiver = userRepository.findById(transfer.receiver())
+				.orElseThrow(() -> new RuntimeException("Receiver not found"));
+
+		if (transfer.sender() == transfer.receiver())
+			throw new RuntimeException("Unauthorized");
+
+		boolean authorized = authorizationService.authorizeTransfer(sender, transfer.amount());
+
+		if (!authorized)
+			throw new RuntimeException("Transfer not authorized, try again later.");
+
+		userService.validateTransfer(sender, transfer.amount());
+
+		Transfer newTransfer = new Transfer();
+		newTransfer.setSender(sender);
+		newTransfer.setReceiver(receiver);
+		newTransfer.setAmount(transfer.amount());
+		transferRepository.save(newTransfer);
+
+		sender.setBalance(sender.getBalance().subtract(transfer.amount()));
+		receiver.setBalance(receiver.getBalance().add(transfer.amount()));
+		userService.saveUser(sender);
+		userService.saveUser(receiver);
+
+		notificationService.notifyUser(sender, transfer.amount(),
+				"You transfered + " + String.valueOf(transfer.amount()) + "to " + String.valueOf(receiver.getName()));
+
+		notificationService.notifyUser(receiver, transfer.amount(),
+				"You received + " + String.valueOf(transfer.amount()) + "from " + String.valueOf(sender.getName()));
+
+		return newTransfer;
+
+	}
+
+}
